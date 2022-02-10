9253ab43a5a65aaa43e3781554f2e8be9f9f0dd2
Radoslav Raikov <greenpule03@gmail.com>
2022-02-10 11:31:51 +0200

    Add files via upload

---
+++ Objects&Classes/06. Inventory.py
@@ -0,0 +1,19 @@
+class Inventory:
+    def __init__(self, capacity):
+        self.__capacity = capacity
+        self.left_capacity = capacity
+        self.items = []
+
+    def add_item(self, item):
+        if self.left_capacity > 0:
+            self.items.append(item)
+            self.left_capacity -= 1
+        else:
+            return "not enough room in the inventory"
+
+    def get_capacity(self):
+        return self.__capacity
+
+    def __repr__(self):
+        return f"Items: {', '.join(self.items)}.\n" \
+               f"Capacity left: {self.left_capacity}"
